e6e960991c215bc28bfa6a1875004182b7e81538
daniel-yili-ye <daniel.ye@live.com>
2024-10-21 20:15:27 -0400

    fix vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/euge-28-bday",
+  base: "/euge-28-bday/",
 });
